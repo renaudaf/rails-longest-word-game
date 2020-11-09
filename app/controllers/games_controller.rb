c884ea9d3620351a3be7fb27fa4fbda7440502a6
renaud archer fortin <renaud.archerf@gmail.com>
2020-11-09 15:56:35 -0500

    everything separated

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,21 +5,29 @@ class GamesController < ApplicationController
  def score
   @word = params[:word]
   @grid = params[:letters]
-  @first = @word.chars.all? { |letter| @word.count(letter) <= @grid.downcase.count(letter) }
 
-  response = open("https://wagon-dictionary.herokuapp.com/#{@word}")
-  json = JSON.parse(response.read)
-  @second = json['found']
-
-  if @first == false
+  if included?(@word) == false
    @answer = "Sorry but #{@word.upcase} can't be built out of #{@grid}"
-  elsif @second == false
+  elsif english?(@word) == false
    @anser = "Sorry but #{@word.upcase} does not seem to be an english word"
-  elsif @first == true && @second == true
-   @answer = "Congratulations, #{@word.upcase} is a valid english word"
+  elsif included?(@word) == true && english?(@word) == true
+   @answer = "Congratulations you earn #{@word.length*10} points, #{@word.upcase} is a valid english word"
   end
  end
 
+ def included?(guess)
+  guess.chars.all? { |letter| guess.count(letter) <= @grid.downcase.count(letter) }  
+ end
+
+ # def score(guess)
+ #  return "#{guess.length*10}"
+ # end
+
+ def english?(word)
+  response = open("https://wagon-dictionary.herokuapp.com/#{@word}")
+  json = JSON.parse(response.read)
+  return json['found']
+end
  def new
   @letters = Array.new(10) { ('A'..'Z').to_a.sample }
  end
